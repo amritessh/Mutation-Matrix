c00a9964fef30475636bf9baaeea569f862fe16f
Amritesh Anand <amriteshanand7@gmail.com>
2025-01-31 17:10:37 -0500

    dependency changes

--- backend/biomarker_prediction_service/src/main/java/com/mutationmatrix/biomarker_prediction_service/BiomarkerPredictionServiceApplication.java
+++ backend/biomarker_prediction_service/src/main/java/com/mutationmatrix/biomarker_prediction_service/BiomarkerPredictionServiceApplication.java
@@ -2,8 +2,13 @@ package com.mutationmatrix.biomarker_prediction_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		BatchAutoConfiguration.class // Add this
+})
 public class BiomarkerPredictionServiceApplication {
 
 	public static void main(String[] args) {
