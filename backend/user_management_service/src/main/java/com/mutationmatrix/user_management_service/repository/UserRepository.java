163ea5f6450730e5dcfa31230262d362e53523d8
Amritesh Anand <amriteshanand7@gmail.com>
2025-02-03 16:49:25 -0500

    completed user repository interface

--- backend/user_management_service/src/main/java/com/mutationmatrix/user_management_service/repository/UserRepository.java
+++ backend/user_management_service/src/main/java/com/mutationmatrix/user_management_service/repository/UserRepository.java
@@ -1,5 +1,16 @@
 package com.mutationmatrix.user_management_service.repository;
 
-public class UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mutationmatrix.user_management_service.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 
 }
