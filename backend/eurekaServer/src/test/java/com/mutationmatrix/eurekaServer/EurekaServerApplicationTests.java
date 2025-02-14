bb5aea2b92ef011e4e3386130edf62637dcddab9
Amritesh Anand <amriteshanand7@gmail.com>
2025-02-14 15:51:51 -0500

    added eureka server and update eureka client configs

---
+++ backend/eurekaServer/src/test/java/com/mutationmatrix/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutationmatrix.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- backend/sequence_upload_service/src/main/java/com/mutationmatrix/sequence_upload_service/SequenceUploadServiceApplication.java
+++ backend/sequence_upload_service/src/main/java/com/mutationmatrix/sequence_upload_service/SequenceUploadServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SequenceUploadServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SequenceUploadServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SequenceUploadServiceApplication.class, args);
+    }
 
 }
